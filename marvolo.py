f454043a9b53b3f57b2aa5c80e091d125d387ce2
malyvsen <5940672+malyvsen@users.noreply.github.com>
2023-05-21 22:25:08 +0200

    feature: model names as markov chains

--- marvolo.py
+++ marvolo.py
@@ -23,12 +23,3 @@ def join_groups(grouped_name: List[str], group_pair: str):
             result.append(grouped_name[idx])
             idx += 1
     return result
-
-
-def iterate_groups(grouped_name):
-    return iter(grouped_name)
-
-
-def iterate_group_pairs(grouped_name):
-    for group_pair in zip(grouped_name[:-1], grouped_name[1:]):
-        yield "".join(group_pair)
